0371b46eaa0aafa2b5b777851e67c25a139bb1a4
Lopes-7 <gui-lopes7@live.com>
2020-08-24 18:50:06 -0300

    Add beverage schema

---
+++ src/models/beverage.js
@@ -0,0 +1,25 @@
+/**
+ * IMPORTS
+ */
+const mongoose = require('mongoose');
+
+
+/**
+ * CODE
+ */
+
+// beverage schema
+const beverageSchema = mongoose.Schema({
+    created_at: {type: Date, default: Date.now},
+    name: {type: String, required: true},
+    net_weight: {type: Number, required: true}, // in ml
+    price: {type: Number, required: true}, // in R$
+    price_per_liter: {type: Number, required: true}, // R$/l
+    vendor: {type: String, required: true}
+});
+
+
+/**
+ * EXPORTS
+ */
+module.exports = mongoose.model('Beverage', beverageSchema);
