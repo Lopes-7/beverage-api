d6fb6e66118fd8714d7d3c2e538e7a852975e804
Lopes-7 <gui-lopes7@live.com>
2020-08-24 18:51:29 -0300

    Add app.js

---
+++ src/app.js
@@ -0,0 +1,32 @@
+/**
+ * IMPORTS
+ */
+const express = require('express');
+
+
+/**
+ * CODE
+ */
+
+// App Controler class
+class AppController {
+    constructor() {
+        this.express = express();
+        this.middlewares();
+        this.routes(); 
+    }
+
+    middlewares() {
+        this.express.use(express.json());
+    }
+
+    routes() {
+        this.express.use(require('./routes'));
+    }
+}
+
+
+/**
+ * EXPORTS
+ */
+module.exports = new AppController().express;
