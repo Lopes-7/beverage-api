2e094926f781db75cb59b59771ae583a925ca738
Guilherme Campello <guilherme.campello@kascosys.com.br>
2021-03-28 13:02:25 -0300

    Change DB connection and routers

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@
  * IMPORTS
  */
 const express = require('express');
+const mongoose = require('mongoose');
 
 /**
  * CODE
@@ -11,8 +12,16 @@ const express = require('express');
 class AppController {
     constructor() {
         this.express = express();
+        this.database();
         this.middlewares();
         this.routes();
+    }
+
+    database() {
+        mongoose
+            .connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true })
+            .then(() => console.log('Connected to database'))
+            .catch(() => console.log('Could not connect to database'));
     }
 
     middlewares() {
@@ -20,7 +29,10 @@ class AppController {
     }
 
     routes() {
-        this.express.use(require('./routes'));
+        this.express.use(require('./routes/beverage'));
+        console.log('Added beverage router');
+        this.express.use(require('./routes/vendor'));
+        console.log('Added vendor router');
     }
 }
 
