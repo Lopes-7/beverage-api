faf6823571d8cbf8b183a283bcdbf97b92245bbd
Guilherme Campello <guilherme.campello@kascosys.com.br>
2021-04-20 21:08:26 -0300

    Vendors router

--- src/routes/vendor.js
+++ src/routes/vendor.js
@@ -10,40 +10,75 @@ const Vendor = require('../models/vendor');
 
 // get list of vendors
 routes.get('/vendors', async (req, res) => {
-    // log request
-    console.log('received request: ', req.method, req.url);
-
     try {
-        const result = await Vendor.find({});
-        res.status(200).send(result);
+        let result = await Vendor.find();
+        res.status(200).json({ 'vendors': result });
     }
     catch (e) {
-        console.log(e);
-        res.status(500).send({ message: 'An error occurred' });
+        console.log('error on request: ', e.message);
+        res.status(500).json({ 'error': { 'status': 500, 'message': 'Internal Server Error' } });
+    }
+});
+
+// get vendor by id
+routes.get('/vendors/:id', async (req, res) => {
+    try {
+        // get parameter id
+        const id = req.params.id;
+
+        let result = await Vendor.findById(id);
+        res.status(200).json({ 'vendor': result });
+    }
+    catch (e) {
+        console.log('error on request: ', e.message);
+        res.status(400).json({ 'error': { 'status': 400, 'message': e.reason.message } });
     }
 });
 
 // post vendor
 routes.post('/vendors', async (req, res) => {
-    // log request
-    console.log('received request: ', req.method, req.url);
-
     try {
-        if (!req.body || !req.body.name || !req.body.city)
-            throw new Error('Bad payload');
+        // invalid body: throw error
+        if (!req.body) {
+            let error = new Error();
+            error.status = 400;
+            error.message = 'Request body not found';
+            throw error;
+        }
 
+        // invalid name: throw error
+        if (!req.body.name) {
+            let error = new Error();
+            error.status = 400;
+            error.message = 'Invalid name';
+            throw error;
+        }
+
+        // invalid city: throw error
+        if (!req.body.city) {
+            let error = new Error();
+            error.status = 400;
+            error.message = 'Invalid city';
+            throw error;
+        }
+
+        // create vendor object
         const vendor = new Vendor({
             address: req.body.address ? req.body.address : '',
             city: req.body.city,
             name: req.body.name,
         });
 
-        const result = await vendor.save();
-        res.status(200).send(result);
+        // save vendor to DB
+        let result = await vendor.save();
+
+        // return response created
+        console.log('vendor created: ', result);
+        res.status(201).json({ 'vendor': result });
     }
     catch (e) {
-        console.log(e);
-        res.status(500).send({ message: 'An error occurred' });
+        console.log('error on request: ', e.message);
+        res.status(400).json({ 'error': { 'status': e.status, 'message': e.message } });
     }
 
 });
